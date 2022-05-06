bc6df0489f179d0cb2fe11aaa826c8709da93744
alessandroCiciarelli <36853940+alessandroCiciarelli@users.noreply.github.com>
2022-05-06 12:13:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from dalle import create_and_show_images
 
 st.title("DALL-E Mini")
 
-text = st.text_input("What should I create?")
+text = st.text_input("Scrivi una parola o frase e l'I.A. crearà delle immagini?")
 
-num_images = st.slider("How many images?", 1, 6)
+num_images = st.slider("Quante immagini vuoi?", 1, 6)
 
-ok = st.button("GO!")
+ok = st.button("Crea!")
 
 if ok:
     create_and_show_images(text, num_images)
