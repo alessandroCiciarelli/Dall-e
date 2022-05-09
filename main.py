4227e2d19cc156c67317083ffac8ac9197712295
alessandroCiciarelli <36853940+alessandroCiciarelli@users.noreply.github.com>
2022-05-09 19:50:22 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -430,7 +430,6 @@ if choose=="Analisi":
         
         if len(linesList) == 1:
             linesList = removeRestrictedCharactersAndWhiteSpaces(linesList)
-            st.write(linesList, selected_timeframe, country_code[0] )
             pytrends.build_payload(linesList, timeframe=selected_timeframe, geo=country_code[0])
             related_queries = pytrends.related_queries()
             temp = pytrends.interest_over_time().drop('isPartial', axis=1)
@@ -438,7 +437,6 @@ if choose=="Analisi":
         
         else:
             linesList = removeRestrictedCharactersAndWhiteSpaces(linesList)
-            st.write(linesList, selected_timeframe, country_code[0] )
             pytrends.build_payload(linesList, timeframe=selected_timeframe, geo=country_code[0])
             related_queries = pytrends.related_queries()
             temp = pytrends.interest_over_time().drop('isPartial', axis=1)
